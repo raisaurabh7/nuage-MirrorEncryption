03dea966cf614b5a605be86748f401daf48918e6
raisaurabh7 <raisaurabh7@gmail.com>
2022-03-11 11:41:20 +0530

    Updated the test case

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import main
 def test_mirror_encryption():
     obj = main.MirrorEncryption()
     test1 = obj.get_mirror("Hello world!")
-    test2 = obj.get_mirror("abc")
+    test2 = obj.get_mirror("Christmas is the 25th of December")
     test3 = obj.get_mirror("ABC1234XYZ")
     test4 = obj.get_mirror("gygyuGyYfyy435##453gfc")
     test5 = obj.get_mirror("^%^%4%$554644354753443#$%#$%%&")
     assert test1 == "Svool dliow!"
-    assert test2 == "Christmas is the 25th of December"
+    assert test2 == "Xsirhgnzh rh gsv 25gs lu Wvxvnyvi"
     assert test3 == "ZYX1234CBA"
     assert test4 == "tbtbfTbBubb435##453tux"
     assert test5 == "^%^%4%$554644354753443#$%#$%%&"
